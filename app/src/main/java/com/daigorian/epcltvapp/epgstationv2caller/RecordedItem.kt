f86b7ac3007ae967effb8b474dea0a9303860204
daigorian <daigorian@daigorian.com>
2021-06-13 13:41:07 +0900

    APIの説明の中で * がついている項目をnon_nullableに設定

--- app/src/main/java/com/daigorian/epcltvapp/epgstationv2caller/RecordedItem.kt
+++ app/src/main/java/com/daigorian/epcltvapp/epgstationv2caller/RecordedItem.kt
@@ -4,13 +4,13 @@ import java.io.Serializable
 
 //録画番組情報
 data class RecordedItem (
-    val id	:Long?, //録画済み番組 id
+    val id	:Long, //録画済み番組 id
     val ruleId	:Long?, //ルール id
     val programId	:Long?, //maximum: 655356553565535 program id
     val channelId	:Long?, //maximum: 6553565535 放送局 id
-    val startAt	:Long?, //時刻 (ms)
-    val endAt	:Long?, //時刻 (ms)
-    val name	:String?, //番組名
+    val startAt	:Long, //時刻 (ms)
+    val endAt	:Long, //時刻 (ms)
+    val name	:String, //番組名
     val description	:String?, //番組詳細
     val extended	:String?, //番組拡張
     val genre1	:Long?, //ジャンル
@@ -25,13 +25,13 @@ data class RecordedItem (
     val videoComponentType	:Long?,
     val audioSamplingRate	:Long?, //番組オーディオサンプリングレート[ 16000, 22050, 24000, 32000, 44100, 48000 ]
     val audioComponentType	:Long?, //
-    val isRecording	:Boolean?, //録画中か
+    val isRecording	:Boolean, //録画中か
     val thumbnails	:List<Long>?, //サムネイル id
     val videoFiles	:List<VideoFile>?,//	ビデオファイル情報
     val dropLog	:DropLogFile?, //ドロップログファイル情報
     val tags	:List<RecordedTag>?,//タグ情報
-    val isEncoding	:Boolean?, //エンコード中か
-    val isProtected	:Boolean?, //自動録画削除対象外か
+    val isEncoding	:Boolean, //エンコード中か
+    val isProtected	:Boolean, //自動録画削除対象外か
 ): Serializable {
 
     override fun toString(): String {
