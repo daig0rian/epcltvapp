f253bbdbf678335d935dede8f25ccd1f47fefb4c
daigorian <daigorian@daigorian.com>
2021-06-27 15:57:09 +0900

    Revert "ほかのFragmentとスタイルを合わせる"
    This reverts commit 3d3f34b4

--- app/src/main/java/com/daigorian/epcltvapp/SettingsActivity.kt
+++ app/src/main/java/com/daigorian/epcltvapp/SettingsActivity.kt
@@ -1,18 +1,13 @@
 package com.daigorian.epcltvapp
 
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
 
-class SettingsActivity : FragmentActivity() {
+class SettingsActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        if (savedInstanceState == null) {
-            fragmentManager.beginTransaction()
-                .replace(R.id.settings_fragment, SettingsFragment())
-                .commit()
 
-        }
     }
 }
