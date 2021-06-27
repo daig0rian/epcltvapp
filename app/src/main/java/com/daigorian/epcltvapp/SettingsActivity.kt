3d3f34b48d927749646e153a44c069f0a25acf2c
daigorian <daigorian@daigorian.com>
2021-06-27 15:05:06 +0900

    ほかのFragmentとスタイルを合わせる

--- app/src/main/java/com/daigorian/epcltvapp/SettingsActivity.kt
+++ app/src/main/java/com/daigorian/epcltvapp/SettingsActivity.kt
@@ -1,13 +1,18 @@
 package com.daigorian.epcltvapp
 
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 
-class SettingsActivity : Activity() {
+class SettingsActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        if (savedInstanceState == null) {
+            fragmentManager.beginTransaction()
+                .replace(R.id.settings_fragment, SettingsFragment())
+                .commit()
 
+        }
     }
 }
